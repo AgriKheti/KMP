142838fcdbb97d9706a4223aeefb0146e274ccf2
agrikheti <gurwinder.agrikheti@gmail.com>
2022-05-12 15:00:59 +0530

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/agrikheti/kmp/Greeting.kt
@@ -0,0 +1,7 @@
+package com.agrikheti.kmp
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/agrikheti/kmp/Platform.kt
@@ -0,0 +1,5 @@
+package com.agrikheti.kmp
+
+expect class Platform() {
+    val platform: String
+}
